c78c5bfde0c184725ad69b67ecedd7c3dfa24f2f
juan <juan24.martinez05@gmail.com>
2018-07-28 21:52:18 -0500

    se agrego jquery popper.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
+
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
